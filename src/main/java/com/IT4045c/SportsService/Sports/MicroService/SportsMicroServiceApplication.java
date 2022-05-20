4ec87007800f2a2a30d706b8caac4067076ddb94
cjohannemann <cjohannemann@gaig.com>
2022-05-20 09:45:49 -0400

    Initial commit

---
+++ src/main/java/com/IT4045c/SportsService/Sports/MicroService/SportsMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.IT4045c.SportsService.Sports.MicroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsMicroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/IT4045c/SportsService/Sports/MicroService/SportsMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IT4045c.SportsService.Sports.MicroService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

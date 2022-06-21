737b0a2fe3c2fff36b661b2867c71cf351c0b8a5
cjohannemann <cjohannemann@gaig.com>
2022-06-21 09:52:43 -0400

    Changed annotation from RestController to Controller to allow HTML to get picked up. Home page now shows on root directory.

--- src/main/java/app/controller/PlayerController.java
+++ src/main/java/app/controller/PlayerController.java
@@ -3,24 +3,24 @@ package app.controller;
 import app.model.Player;
 import app.service.PlayerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 import java.util.List;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class PlayerController {
 
     private final PlayerService playerService;
 
     @RequestMapping("/")
-    public String index() {return "start";}
+    public String index() {return "index";}
 
     @GetMapping("/getAllPlayers")
     public Collection<Player> getAll() {

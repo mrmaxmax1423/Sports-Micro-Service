439542abf1b77ddaf8836152aeb1a4fe8c7a8576
cjohannemann <cjohannemann@gaig.com>
2022-06-21 09:31:25 -0400

    Added Start to the PlayerController.

--- src/main/java/app/controller/PlayerController.java
+++ src/main/java/app/controller/PlayerController.java
@@ -4,17 +4,23 @@ import app.model.Player;
 import app.service.PlayerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class PlayerController {
 
     private final PlayerService playerService;
+
+    @RequestMapping("/")
+    public String index() {return "start";}
 
     @GetMapping("/getAllPlayers")
     public Collection<Player> getAll() {
@@ -26,10 +32,11 @@ public class PlayerController {
 
         return playerService.savePlayer(player);
     }
-//
-//    @GetMapping("/getPlayer/{playerLastName}")
-//    public List<Player> getPlayerByLastName(@PathVariable String inPlayerName){
-//        return repository.findPlayerByLastName(inPlayerName);
-//    }
+
+    @GetMapping("/getPlayer/{playerLastName}/")
+    public List<Player> getPlayerByLastName(@PathVariable("playerLastName") String inPlayerName){
+
+        return playerService.findPlayerByLastName(inPlayerName);
+    }
 
 }

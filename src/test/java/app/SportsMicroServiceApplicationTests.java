c7c51b65641a4fb3de238e24df72fe91fafab579
Eric Miller <eldmilleree@gmail.com>
2022-06-30 14:40:09 -0400

    Created First Test

--- src/test/java/app/SportsMicroServiceApplicationTests.java
+++ src/test/java/app/SportsMicroServiceApplicationTests.java
@@ -8,9 +8,52 @@ class SportsMicroServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void fetchWinsLossesUpcomingGame(){
+		givenBaseballButtonAvailable();
+		givenFootballButtonAvailable();
+		givenSoccerButtonAvailable();
+		whenSearchTeamWin();
+		whenSearchTeamLoss();
+		whenSearchTeamUpcomingGame();
+		thenReturnWin();
+		thenReturnLoss();
+		thenReturnUpcomingGame();
+	}
+
+	private void givenSoccerButtonAvailable() {
+	}
+
+	private void givenFootballButtonAvailable() {
+		
+	}
+
+	private void givenBaseballButtonAvailable() {
 
 	}
 
+	private void whenSearchTeamUpcomingGame() {
+	}
+
+	private void whenSearchTeamLoss() {
+		
+	}
+
+	private void whenSearchTeamWin() {
+	}
+
+	private void thenReturnLoss() {
+	}
+
+	private void thenReturnUpcomingGame() {
+		
+	}
+
+	private void thenReturnWin() {
+		
+	}
 
 
 }

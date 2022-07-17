745ff1653fb13b3c05ada77b95e1aa1a7cbd3f3d
bartrn <89704134+bartrn@users.noreply.github.com>
2022-07-17 15:12:20 -0400

    fix player added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,8 +3,8 @@ INSERT INTO PLAYER (id,firstName, lastName, hometown, dob, teamPlayingFor, sport
 (2,'Joe', 'Dimagio', 'Union, KY', '10-03-1985', 'Cincinnati Reds', 'baseball'),
 (3,'Hank', 'Aaron', 'Union, KY', '10-03-1984', 'Cincinnati Reds', 'baseball'),
 (4,'Katie', 'Johannemann', 'Union, KY', '10-03-1983', 'Cincinnati Reds', 'baseball'),
-(5,'Chase','Young','Upper Marlboro,Maryland','04-14-1999','Washington','Football');
-(6,'Brenner','Forward','Cuiabá Brazil','01-16-2000','fc cincinnati','Soccer')
+(5,'Chase','Young','Upper Marlboro,Maryland','04-14-1999','Washington','Football'),
+(6,'Brenner','Forward','Cuiabá Brazil','01-16-2000','fc cincinnati','Soccer');
 Insert into MLB (sport, team, wins, losses, nextGame) values
 ('Baseball', 'Cincinnati Reds', '8', '25', '05-25-2022'),
 ('Baseball', 'Cincinnati Reds', '8', '25', '05-26-2022');

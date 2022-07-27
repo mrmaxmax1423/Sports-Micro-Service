fabcbfdf62a7133b1b8a0fd1c262dd983d8aa447
bartrn <89704134+bartrn@users.noreply.github.com>
2022-07-27 16:12:40 -0400

    Update MLS.java

--- src/main/java/app/model/MLS.java
+++ src/main/java/app/model/MLS.java
@@ -1,8 +1,12 @@
 package app.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MLS {
     private String sport;
     private String team;
